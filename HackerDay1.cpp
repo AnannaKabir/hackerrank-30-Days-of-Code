722475c85951de7f281985d2a54da367d5e0aaf7
Moumita Kabir Ananna <45585390+AnannaKabir@users.noreply.github.com>
2019-11-24 16:51:21 +0600

    Update HackerDay1.cpp

--- HackerDay1.cpp
+++ HackerDay1.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
-#include <sstream>
+#include <iomanip>
+#include <limits>
+
 using namespace std;
-int main()
-{
-    string name = "Bill";
-    stringstream ss;
-    ss << "Your name is: " << name;
-    string info = ss.str();
-    cout << info << endl;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+   int x;
+   double y;
+   string  z;
+   string tmp;
+   getline(cin,tmp);
+   x = stoi(tmp);
+   getline(cin, tmp);
+   y= stod(tmp);
+   getline(cin, z);
+
+   cin>>x>>y;
+
+printf("%i\n",i+x);
+printf("%.1f\n",d+y);
+   cout<< s+z<<endl;
+
     return 0;
 }
